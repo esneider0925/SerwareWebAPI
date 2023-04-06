2fe0cb33088a299e169bf6cfd5bd641b5a028691
Garcia, Esneider <Esneider.Garcia@dieboldnixdorf.com>
2023-04-06 12:32:50 -0500

    Se crea proyecto de autenticacion y manejo de usuarios para el resto de la plataforma

---
+++ SerwareWebApi/SerwareWebAutheticationApi/Model/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerwareWebAutheticationApi.Model
+{
+    public class User
+    {
+        private string uuid;
+        private string username;
+        private string password;
+        private UserType userType;
+        private UserConfig userConfig;
+        public User(string uuid, string username, string password, UserType userType, UserConfig userConfig)
+        {
+            this.uuid = uuid;
+            this.username = username;
+            this.password = password;
+            this.userType = userType;
+            this.userConfig = userConfig;
+        }
+
+
+    }
+}
---
+++ SerwareWebApi/SerwareWebAutheticationApi/Model/UserType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerwareWebAutheticationApi.Model
+{
+    public class UserType
+    {
+
+    }
+}
